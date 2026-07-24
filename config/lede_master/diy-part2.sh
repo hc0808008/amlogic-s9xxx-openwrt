76d34b04e1f6923cb67d83d75538c3aac82e58d2
hc0808008 <133383340+hc0808008@users.noreply.github.com>
2026-07-25 04:00:42 +0800

    更新插件列表

--- config/lede_master/diy-part2.sh
+++ config/lede_master/diy-part2.sh
@@ -56,3 +56,59 @@ git clone -b main https://github.com/ophub/luci-app-amlogic.git package/luci-app
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
 # ------------------------------- Other ends -------------------------------
+
+# Dockerman(Docker管理面板)
+git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+
+# OpenClash
+git clone https://github.com/vernesong/OpenClash package/luci-app-openclash
+
+# ttyd 网页终端
+git clone https://github.com/lisaac/luci-app-ttyd package/luci-app-ttyd
+
+# 解锁网易云音乐
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic package/luci-app-unblockneteasemusic
+
+# Samba4 文件共享
+git clone https://github.com/lisaac/luci-app-samba4 package/luci-app-samba4
+
+# 分区扩容
+git clone https://github.com/lisaac/luci-app-partexp package/luci-app-partexp
+
+# quickfile 文件管理器
+git clone https://github.com/lisaac/luci-app-quickfile package/luci-app-quickfile
+
+# USB打印机服务
+git clone https://github.com/lisaac/luci-app-p910nd package/luci-app-p910nd
+
+# 磁盘管理
+git clone https://github.com/lisaac/luci-app-diskman package/luci-app-diskman
+
+# Aria2下载工具
+git clone https://github.com/lisaac/luci-app-aria2 package/luci-app-aria2
+
+# AP‑Modem 访问光猫AP
+git clone https://github.com/lisaac/luci-app-ap-modem package/luci-app-ap-modem
+
+# Alist网盘管理
+git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+
+# FRPC内网穿透客户端
+git clone https://github.com/kuoruan/luci-app-frpc package/luci-app-frpc
+
+# ZeroTier异地组网
+git clone https://github.com/lisaac/luci-app-zerotier package/luci-app-zerotier
+
+# DDNS动态域名
+git clone https://github.com/lisaac/luci-app-ddns package/luci-app-ddns
+
+# SmartDNS域名加速解析
+git clone https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+
+# Argon美化主题（成熟稳定）
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+
+# iStore 软件中心（仅商店面板，不整套UI）
+git clone https://github.com/linkease/istore package/istore
+cp -r package/istore/luci/luci-app-store package/luci-app-store
